e4d017d840b58aca21c86210e1b099f2f3dcbdaf
Arthur <arthurcs426@gmail.com>
2019-09-28 10:51:34 -0700

    transitioned to webpack

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Switch, Link, Route, HashRouter, NavLink} from 'react-router-dom';
+
+const Home = () =><h1>Homepage</h1>;
+
+const Nav = () =>{
+  const links = [
+    { text: 'Home', to: '/'},
+    { text: 'Events', to: '/events'},
+  ];
+  return(
+    <nav>
+      {
+        links.map(link =>
+          <NavLink key = {link.text} to = {link.to}> {link.text}</NavLink>
+        )
+      }
+      </nav>
+  )
+};
+
+const Events = () =><h1>Eventspage</h1>;
+
+class App extends React.Component{
+  componentDidMount(){
+    fetchEvents();
+  }
+
+  render(){
+    return (
+      <HashRouter>
+        <h1>Acme Event Planner With Redux</h1>
+        <Route component = {Nav} />
+        <Route path = '/' component = {Home} exact/>
+        <Route path = '/events' component = {Events} />
+      </HashRouter>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/connect.js
@@ -0,0 +1,25 @@
+import store from './store';
+import React from 'react';
+
+const connect = (Component)=> {
+  class Connected extends React.Component{
+    constructor(){
+      super();
+      this.state = store.getState();
+    }
+    componentWillUnmount(){
+      this.unsubscribe();
+    }
+    componentDidMount(){
+      this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+    }
+    render(){
+      return (
+        <Component {...this.state } {...this.props }/>
+      );
+    }
+  }
+  return Connected;
+}
+
+export default connect;
